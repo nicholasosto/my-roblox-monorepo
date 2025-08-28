0919a1aec720be451a5fe7c897f0683106a3c18c
nicholasosto <nicholas.osto@gmail.com>
2025-08-28 13:59:20 -0400

    feat: add clamp utility function and update client logic to use it

--- packages/game/src/client/main.client.ts
+++ packages/game/src/client/main.client.ts
@@ -1,5 +1,8 @@
 // Minimal rbxts client entry
 print("Hello from client! SOOsOO COwwOOOL 22");
 import { MAGIC_NUMBER } from "./client-helpers";
-import { clamp } from "@my-roblox-monorepo/rbxts-utils"
+import { clamp } from "../shared/utils"
 print(MAGIC_NUMBER);
+
+const otherNumber = clamp(MAGIC_NUMBER + 10, 0, 100);
+print(otherNumber);
