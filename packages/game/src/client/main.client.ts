c37b766afe4f795fcefe8b26d959a72cdf8c833a
nicholasosto <nicholas.osto@gmail.com>
2025-08-28 13:22:32 -0400

    feat: update package dependencies and add MAGIC_NUMBER constant

--- packages/game/src/client/main.client.ts
+++ packages/game/src/client/main.client.ts
@@ -1,2 +1,4 @@
 // Minimal rbxts client entry
-print("Hello from client! SOOsOO COOOOL 2");
+print("Hello from client! SOOsOO COwwOOOL 22");
+import { MAGIC_NUMBER } from "./client-helpers";
+print(MAGIC_NUMBER);
