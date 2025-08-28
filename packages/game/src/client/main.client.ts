f8a93f0b3e2d3223c1a92269759060bbbcdf9c99
nicholasosto <nicholas.osto@gmail.com>
2025-08-28 13:34:29 -0400

    feat: add rbxts-utils dependency and update tsconfig paths

--- packages/game/src/client/main.client.ts
+++ packages/game/src/client/main.client.ts
@@ -1,4 +1,5 @@
 // Minimal rbxts client entry
 print("Hello from client! SOOsOO COwwOOOL 22");
 import { MAGIC_NUMBER } from "./client-helpers";
+import { clamp } from "@my-roblox-monorepo/rbxts-utils"
 print(MAGIC_NUMBER);
